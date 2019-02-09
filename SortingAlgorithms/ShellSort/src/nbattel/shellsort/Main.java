a2f7443016a0083d47b8447515511174cb67b318
nbattel <nbattel@uwo.ca>
2019-02-09 17:42:45 -0500

    Completed Shell Sort

---
+++ SortingAlgorithms/ShellSort/src/nbattel/shellsort/Main.java
@@ -0,0 +1,32 @@
+package nbattel.shellsort;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int[] array = {20, 56, 87, 3, 10, -7, 77};
+
+        for (int gap = array.length/2; gap > 0; gap--)
+        {
+            for(int i = gap; i <array.length; i++)
+            {
+                int newElement = array[i];
+                int j = i;
+
+                while(j >= gap && array[j - gap] > newElement)
+                {
+                    array[j] = array[j - gap];
+                    j -= gap;
+                }
+
+                array[j] = newElement;
+            }
+        }
+
+        for (int i = 0; i < args.length; i++)
+        {
+            System.out.println(array[i]);
+        }
+    }
+}
