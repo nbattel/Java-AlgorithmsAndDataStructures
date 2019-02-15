e6ef86c93a872fbf708b7baa71eaf2a5d9cad757
nbattel <nbattel@uwo.ca>
2019-02-14 21:01:34 -0500

    Completed Singly Linked List

--- Lists/SinglyLinkedList/src/nbattel/SinglyLinkedList/Main.java
+++ Lists/SinglyLinkedList/src/nbattel/SinglyLinkedList/Main.java
@@ -2,7 +2,27 @@ package nbattel.SinglyLinkedList;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+
+        EmployeeLinkedList list = new EmployeeLinkedList();
+        list.addToFront(janeJones);
+        list.addToFront(johnDoe);
+        list.addToFront(marySmith);
+        list.addToFront(mikeWilson);
+
+        System.out.println(list.getSize());
+
+        list.printList();
+
+        list.removedFromFront();
+
+        System.out.println(list.getSize());
+
+        list.printList();
     }
 }
