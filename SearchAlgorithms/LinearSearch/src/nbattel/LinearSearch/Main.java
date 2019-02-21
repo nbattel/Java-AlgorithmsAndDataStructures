4b3e84d2bc1b4de075f820cd46ca6c58678b9e2a
nbattel <nbattel@uwo.ca>
2019-02-21 18:04:27 -0500

    Completed Linear Search

---
+++ SearchAlgorithms/LinearSearch/src/nbattel/LinearSearch/Main.java
@@ -0,0 +1,27 @@
+package nbattel.LinearSearch;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        System.out.println(linearSearch(intArray, -15));
+        System.out.println(linearSearch(intArray, 1));
+        System.out.println(linearSearch(intArray, 8888));
+        System.out.println(linearSearch(intArray, -22));
+    }
+
+    public static int linearSearch(int[] input, int value)
+    {
+        for(int i = 0; i < input.length; i++)
+        {
+            if(value == input[i])
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
