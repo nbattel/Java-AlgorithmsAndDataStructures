0b11189ddf416a0d2574c552a5e6212a9756bfd2
nbattel <nbattel@uwo.ca>
2019-02-28 16:54:05 -0500

    Completed Binary Search Trees Challenge #1

--- BinarySearchTrees/BinarySearchTreesChallenge#1/src/nbattel/BinarySearchTreesChallenge1/Main.java
+++ BinarySearchTrees/BinarySearchTreesChallenge#1/src/nbattel/BinarySearchTreesChallenge1/Main.java
@@ -1,8 +1,38 @@
+/*
+- Add pre-order traversal to our Tree Implementation
+- Use the starter project in the resources area
+ */
+
 package nbattel.BinarySearchTreesChallenge1;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        Tree intTree = new Tree();
+        intTree.insert(25);
+        intTree.insert(20);
+        intTree.insert(15);
+        intTree.insert(27);
+        intTree.insert(30);
+        intTree.insert(29);
+        intTree.insert(26);
+        intTree.insert(22);
+        intTree.insert(32);
+        intTree.insert(17);
+
+        intTree.traverseInOrder();
+        System.out.println();
+
+        intTree.traversePreOrder();
+        System.out.println();
+
+//        System.out.println(intTree.get(27));
+//        System.out.println(intTree.get(17));
+//        System.out.println(intTree.get(8888));
+
+//        System.out.println(intTree.min());
+//        System.out.println(intTree.max());
+
     }
 }
